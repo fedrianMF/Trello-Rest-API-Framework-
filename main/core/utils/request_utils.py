f61a8da6f708d8f812b285027622b7c8be8154fb
Felix Mamani <felix.mamani@fundacion-jala.org>
2020-05-13 20:01:09 -0400

    feat(utils): add to request_utils convert vaules function

---
+++ main/core/utils/request_utils.py
@@ -0,0 +1,30 @@
+class RequestUtils:
+    """Class defined to request Utils"""
+
+    @staticmethod
+    def generate_data(body):
+        data = {}
+        if body is not None:
+            for row in body:
+                if row['value'] == 'True' or row['value'] == 'False':
+                    data[str(row['key'])] = eval(row['value'])
+                elif row['value'].isdigit():
+                    data[str(row['key'])] = int(row['value'])
+                elif is_float(row['value']):
+                    data[str(row['key'])] = float(row['value'])
+                else:
+                    data[str(row['key'])] = row['value']
+        return data
+
+    @staticmethod
+    def validate_body(body, expected_data):
+        # validate body here
+        pass
+
+    @staticmethod
+    def is_float(value):
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
