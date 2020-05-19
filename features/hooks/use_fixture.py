2903e255790cbcb20b1adbbadc7cdd169d6ca10c
Felix Mamani <felix.mamani@fundacion-jala.org>
2020-05-19 14:40:29 -0400

    refac(hook): added post, delete hooks

--- features/hooks/use_fixture.py
+++ features/hooks/use_fixture.py
@@ -4,6 +4,7 @@ from main.trello.utils.boards_api import BoardsAPI
 from main.trello.utils.member_api import MemberAPI
 from main.trello.utils.lists_api import ListsAPI
 from main.trello.utils.cards_api import CardsAPI
+from main.trello.utils.api_constants import HttpMethods
 from main.core.request_manager import RequestsManager as RM
 
 
@@ -15,8 +16,7 @@ def use_fixture_by_tag(tag, context):  # pylint: disable=W0613
     """
     if 'create.' in tag:
         if 'board' in tag:
-            context.board_id = BoardsAPI.create_board("test board create at before tags",
-                                                      "test description board")
+            context.board_id = BoardsAPI.create_board()
         elif 'list' in tag:
             context.list_id = ListsAPI.create_list(context.board_id)
         elif 'card' in tag:
@@ -42,11 +42,58 @@ def use_fixture_by_tag(tag, context):  # pylint: disable=W0613
             context.newuser_id = context.info_user['id']
 
 
-    def post_resource(context, endpoint, tag, **kwargs):
+    def post_resource(tag, context):
         """Basic hook to create board, list or card
 
         :param context: Global context from behave
         :type context: obj
         :param tag: tag to be retrieved
         """
-        RM.get_instance().do_request()
+        type_data = tag.split('.')[-1]
+        body = {
+            "name": "test " + type_data +" create at before tags", 
+        }
+        endpoint = "/" + type_data + "/"
+        if type_data == 'board':
+            body['desc'] = "Test description Board"
+            json_response = RM.get_instance().do_request(HttpMethods.POST.value, endpoint, body=body)
+            context.id_dictionary[type_data] = json_response["id"]
+        elif type_data == 'list':
+            body['idBoard'] = context.id_dictionary['board']
+            json_response = RM.get_instance().do_request(HttpMethods.POST.value, endpoint, body=body)
+            context.id_dictionary[type_data] = json_response["id"]
+        else:
+            body['idList'] = context.id_dictionary['list']
+            json_response = RM.get_instance().do_request(HttpMethods.POST.value, endpoint, body=body)
+            context.id_dictionary[type_data] = json_response["id"]
+
+
+    def delete_resource(tag, context):
+        """Basic hook to delete board, list or card
+
+        :param context: Global context from behave
+        :type context: obj
+        :param tag: tag to be retrieved
+        """
+        type_data = tag.split('.')[-1]
+        endpoint = "/" + type_data + "/" + context.id_dictionary[type_data]
+        RM.get_instance().do_request(HttpMethods.DELETE.value, endpoint)
+
+
+    def get_resource(tag, context):
+        """Basic hook to delete board, list or card
+
+        :param context: Global context from behave
+        :type context: obj
+        :param tag: tag to be retrieved
+        """
+        type_data = tag.split('.')[-1]
+        if type_data == 'board':
+            endpoint = "/" + type_data + "/" + context.id_dictionary[type_data]
+            RM.get_instance().do_request(HttpMethods.DELETE.value, endpoint)
+        elif type_data == 'list':
+            endpoint = "/" + type_data + "/" + context.id_dictionary[type_data]
+            RM.get_instance().do_request(HttpMethods.DELETE.value, endpoint)
+        else:
+            endpoint = "/" + type_data + "/" + context.id_dictionary[type_data]
+            RM.get_instance().do_request(HttpMethods.DELETE.value, endpoint)
